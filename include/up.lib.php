68f576339736043802425a8a18121f6485353004
Peter Lieverdink <me@cafuego.net>
2015-05-15 09:26:28 +1000

    Tidy up API, configure types, error handling.

--- include/up.lib.php
+++ include/up.lib.php
@@ -35,7 +35,7 @@ class JawboneAuth {
     $params = array(
       'response_type' => 'code',
       'client_id'     => $this->client_id,
-      'scope'         => 'basic_read extended_read move_read sleep_read',
+      'scope'         => $this->access_string(),
       'redirect_uri'  => url('up/auth', array('absolute' => TRUE))
     );
     return url(UP_AUTH_URI, array('query' => $params, 'absolute' => TRUE));
@@ -62,26 +62,80 @@ class JawboneAuth {
     if ($response->code < 200 || $response->code >= 300) {
       watchdog('up', 'A Jawbone UP API error occurred: @error', array('@error' => $response->error), WATCHDOG_ERROR);
       drupal_set_message(t('A Jawbone UP API error occurred: %error', array('%error' => $response->error)), 'error');
-      return NULL;
+      throw new JawboneAuthException($response->error);
     }
 
     // Thus an OK response. Whee!
     return json_decode($response->data);
   }
+
+  /**
+   * Create a permissions string to access enabled types.
+   */
+  private function access_string() {
+    $permissions = array();
+    $types = up_summary_types(NULL, NULL, TRUE);
+
+    foreach ($types as $type) {
+      $permissions[] = $type['permission'];
+    }
+
+    return implode(' ', $permissions);
+  }
 }
 
 /**
  * Return a list of summary types and sub-types that we can deal with.
+ *
+ * @param String $type
+ *   Return only info for this type.
+ * @param Integer sub_type
+ *   Returen only info for this sub_type.
+ * @param Boolean $filter
+ *   Return only info for enabled types.
+ *
+ * @return
+ *   An multi-dimension array, or a string, or NULL.
  */
-function up_summary_types($type = NULL, $sub_type = NULL) {
+function up_summary_types($type = NULL, $sub_type = NULL, $filter = FALSE) {
+
   $types = array(
+    'user' => array(
+      'endpoint' => 'user',
+      'permission' => 'basic_read',
+      'title' => t('User'),
+      'sub_types' => array(),
+    ),
+    'goal' => array(
+      'endpoint' => 'goals',
+      'permission' => '',
+      'title' => t('Goals'),
+      'sub_types' => array(),
+    ),
+    'mood' => array(
+      'endpoint' => 'mood',
+      'permission' => 'read_mood',
+      'title' => t('Mood'),
+      'sub_types' => array(
+        1 => t('Amazing'),
+        2 => t('Pumped UP'),
+        3 => t('Energized'),
+        4 => t('Meh'),
+        5 => t('Dragging,'),
+        6 => t('Exhausted'),
+        7 => t('Totally Done'),
+        8 => t('Good'),
+      ),
+    ),
     'move' => array(
       'endpoint' => 'moves',
+      'permission' => 'move_read',
       'title' => t('Move'),
       'sub_types' => array(),
     ),
     'sleep' => array(
       'endpoint' => 'sleeps',
+      'permission' => 'sleep_read',
       'title' => t('Sleep'),
       'sub_types' => array(
         0 => t('Normal'),
@@ -91,17 +145,26 @@ function up_summary_types($type = NULL, $sub_type = NULL) {
     ),
   );
 
+  // Return all info for all enabled types.
+  if (!empty($filter)) {
+    return array_intersect_key($types, array_filter(variable_get('up_types', array())));
+  }
+
+  // Return all info for all types.
   if (empty($type)) {
     return $types;
   }
 
+  // You asked for what? 404 not found.
   if (empty($types[$type])) {
     return NULL;
   }
 
+  // Return all info for the specified type.
   if ($sub_type == NULL) {
     return $types[$type];
   }
 
-  return $types[$type]['sub_types'][$sub_type];
+  // Return the name of the sub-type.
+  return (!empty($types[$type]['sub_types'][$sub_type])) ? $types[$type]['sub_types'][$sub_type] : NULL;
 }
